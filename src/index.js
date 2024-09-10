113d5bf72376f6c507510fcca083b4a5fdbd46dc
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-09-10 00:25:27 -0600

    add errorhandling 2 getWeather & get percise data
    can now throw error if there is any and now
    getting more specific data from the api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 async function getWeather(zip) {
-  const url =
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${zip}?key=R9YNYGYLHKQAZ4B8BR72HZ678`;
-  const result = await fetch(url, {
-    mode: 'cors',
-  });
-  const data = await result.json();
-  console.log(data);
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${zip}?key=R9YNYGYLHKQAZ4B8BR72HZ678`;
+    const result = await fetch(url, {
+      mode: 'cors',
+    });
+    const data = await result.json();
+    return data;
+  } catch (e) {
+    throw new Error(`Error in getWeather: ${e}`);
+  }
 }
 
-getWeather('79928');
+
+
+getWeather('79928')
+  .then( (data) => {
+    console.log(data.description);    
+  })
+  .catch( (e) =>{
+    throw new Error(e);
+});
+
+
