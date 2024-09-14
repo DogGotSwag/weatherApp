7f03409f645828c94a1b4686e1a7598b1eb427c7
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-09-13 22:49:20 -0600

    set pattern for zipcode & add eventLister 2 button
    pattern will allow form to check if user input is
    valid zip code, button is where we will check and
    use input for the getWeather function

--- src/index.js
+++ src/index.js
@@ -13,15 +13,25 @@ async function getWeather(zip) {
   }
 }
 
+// getWeather('79928')
+//   .then( (data) => {
+//     console.log(data.description);
+//   })
+//   .catch( (e) =>{
+//     throw new Error(e);
+// });
 
-
-getWeather('79928')
-  .then( (data) => {
-    console.log(data.description);    
-  })
-  .catch( (e) =>{
-    throw new Error(e);
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+form.addEventListener('submit', (event) => {  
+  event.preventDefault();
+  if(!input.validity.valid){
+    input.setCustomValidity('Use U.S postal code plz');
+    input.reportValidity();
+    
+  }
+  else{
+    input.setCustomValidity('');        
+  }
+  
 });
-
-console.log("Hello");
-
