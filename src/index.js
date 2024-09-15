b31d87df2b0fe1486bc8bbc0bb8ac8431f66d621
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-09-15 17:33:16 -0600

    add html to display gif make function 4 gif
    made the area to display the gif and use function
    to get img for it added css for structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import './style.css';
 import getWeather from './getWeather';
 
+function intialGif() {
+  const img = document.querySelector('img');
+  fetch(
+    'https://api.giphy.com/v1/gifs/translate?api_key=84bbjIY88igM5FQdGqNXKmW0A5ajiRxH&s=start',
+    { mode: 'cors' }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    });
+
+}
+
+intialGif();
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 form.addEventListener('submit', (event) => {
